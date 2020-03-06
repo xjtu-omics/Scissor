562b24ca77578c1f7bacd453e4e1772b154872c1
jiadong324 <jdlin910324@gmail.com>
2020-03-06 11:13:17 +0100

    new updates

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(
     author='Jiadong Lin, Songbo Wang',
     author_email='jiadong324@gmail.com',
     description='A complex genome rearrangement simulator',
-    requires=['python (>= 3.6)']
+    requires=['python (>= 3.6)'],
+    entry_points={'console_scripts': ['Scissor=Scissor.Scissor:main']}
 )
