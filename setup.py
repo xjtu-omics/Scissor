87e05600a97f0251a8f28bee8efafdd7c5d800ad
jiadong324 <jdlin910324@gmail.com>
2020-03-06 11:27:04 +0100

    new updates

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author_email='jiadong324@gmail.com',
     description='A complex genome rearrangement simulator',
     requires=['python (>= 3.6)'],
-    entry_points={'console_scripts': ['Scissor=Scissor.Scissor:main']}
+    entry_points={'console_scripts': ['Scissor=Scissor:main']}
 )
